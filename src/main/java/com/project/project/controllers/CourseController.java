5e90c127f447d9915602e26aa770fce4f01380e6
JustCodeandChill <inforreceiver@gmail.com>
2024-03-18 20:20:01 -0400

    separate footer and header element to new file

---
+++ src/main/java/com/project/project/controllers/CourseController.java
@@ -0,0 +1,13 @@
+//package com.project.project.controllers;
+//
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class CourseController {
+//    @RequestMapping(value = "/courses")
+//    public String course() {
+//        return "courses.html";
+//    }
+//}
