384b7505c2bb6b0563716527d2fca7229606a09b
JustCodeandChill <inforreceiver@gmail.com>
2024-04-23 00:06:30 -0400

    add controller to submit form

--- src/main/java/com/project/project/controllers/ContactController.java
+++ src/main/java/com/project/project/controllers/ContactController.java
@@ -2,11 +2,27 @@ package com.project.project.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class ContactController {
     @RequestMapping("/contact")
     public String displayContactPage() {
         return "contact.html";
+    }
+
+    @RequestMapping(value = "/saveMsg", method = POST)
+    public ModelAndView saveMessage(@RequestParam(name = "name") String name,
+                                    @RequestParam String mobileNum,
+                                    @RequestParam String email,
+                                    @RequestParam String subject,
+                                    @RequestParam String message) {
+        System.out.println(message);
+        return new ModelAndView("redirect:/contact");
+
+
     }
 }
