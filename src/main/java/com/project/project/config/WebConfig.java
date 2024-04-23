fbfee7209a4f9e1154e88ec94cafca638b7b62fd
JustCodeandChill <inforreceiver@gmail.com>
2024-04-22 22:17:51 -0400

    add link to about page in homepage

--- src/main/java/com/project/project/config/WebConfig.java
+++ src/main/java/com/project/project/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("courses").setViewName("courses");
+        registry.addViewController("/courses").setViewName("courses");
+        registry.addViewController("/about").setViewName("about");
     }
 }
